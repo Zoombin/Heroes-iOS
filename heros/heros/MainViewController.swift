d20293dbfeb9e5a184ceaba59525192b8031738e
ycabc1989 <yc@ycdeMac.local>
2015-05-08 17:11:20 +0800

    提交资源文件

--- heros/heros/MainViewController.swift
+++ heros/heros/MainViewController.swift
@@ -8,25 +8,33 @@
 
 import UIKit
 
-let scrollView = UIScrollView()
+let topScrollView = UIScrollView()
 let pageControl = UIPageControl()
-class MainViewController: UIViewController {
+
+class MainViewController: UIViewController, UIScrollViewDelegate {
    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationController?.navigationBarHidden = true;
+        self.navigationController?.navigationBarHidden = true
+        
         let screenWidth = UIScreen .mainScreen().bounds.size.width
-        let rect = CGRectMake(0, 0, screenWidth, 300)
-        scrollView.frame = rect
-        scrollView.backgroundColor = UIColor .lightGrayColor()
-        scrollView.contentSize = CGSizeMake(screenWidth * 3, 0)
-        scrollView.pagingEnabled = true
-        self.view.addSubview(scrollView)
+        let screenHeight = UIScreen .mainScreen().bounds.size.height
+        
+        let rect = CGRectMake(0, 0, screenWidth, screenHeight * 2/3)
+        
+        topScrollView.frame = rect
+        topScrollView.backgroundColor = UIColor .lightGrayColor()
+        topScrollView.contentSize = CGSizeMake(screenWidth * 3, 0)
+        topScrollView.pagingEnabled = true
+        topScrollView.delegate = self
+        self.view.addSubview(topScrollView)
         
         let pageControlFrame = CGRectMake(0, CGRectGetMaxY(rect) - 20, rect.size.width, 20)
         pageControl.frame = pageControlFrame
         pageControl.numberOfPages = 3
         self.view.addSubview(pageControl)
+        
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -34,6 +42,11 @@ class MainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+       let page = (scrollView.contentOffset.x / scrollView.bounds.size.width)
+        pageControl.currentPage = Int(page)
+        println(pageControl.currentPage)
+    }
     
 }
 
